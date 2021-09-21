860d9f153596731b5bce2d18e18a955695395948
maciek <maciej.pozniak@o2.pl>
2021-09-21 19:55:37 +0200

    druga_opcja

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+""""
 import sys
 
 A=int(input("Podaj a: "))
@@ -15,3 +16,24 @@ elif delta >0:
     print("Funkcja ma dwa miejsca zerowe: " ,{x1} ,"i" ,{x2})
 else:
     print("Delta mnijesza od zera - brak rozwiązań")
+"""
+
+import sys
+import math
+print(sys.argv)
+
+A=int(sys.argv[1])
+B=int(sys.argv[2])
+C=int(sys.argv[3])
+
+delta=math.pow(B,2)-(4*A*C)
+
+if(delta < 0):
+    print("Nie ma miejsc zerowych")
+elif(delta==0):
+    x1=-B/(2*A)
+    print(f"Funkcja zawiera 1 miejsce zerowe: {x1}")
+else:
+    x1=(-B-math.sqrt(delta))/(2*A)
+    x2=(-B+math.sqrt(delta))/(2*A)
+    print(f"Funkcja zawiera 2 miejsca zerowe: {x1} oraz {x2}")
