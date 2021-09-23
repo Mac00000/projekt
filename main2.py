00a1170ba7f564ee9db76b01c7681f24a51dbeb4
maciek <maciej.pozniak@o2.pl>
2021-09-23 17:09:21 +0200

    aaa

---
+++ main2.py
@@ -0,0 +1,46 @@
+'''
+Napisz program, który pozwoli na prównanie stosunku ilości pizzy do ceny pomiędzy trzema pizzami.
+Znajdź restaurację w Twojej okolicy i za pomocą wbudowanej biblioteki sys wprowadź do programu odpowiednie dane a następnie
+wyświetl informacje w postaci listy stringów zawierającą następujące informacje:
+
+Przykład elementu wyświetlanej listy:
+        Restauracja: <nazwa_restauracji>, pizza: <nazwa_pizzy>, rozmiar: <rozmiar_pizzy>, cena: <cena_pizzy>
+
+Dodatkowo korzystając z instrukcji warunkowych sprawdź, która pizza ma najlepszy stosunek cenu do ilości i wyświetl informacje tej pizzy podobnie jak w powyższym przykładzie.
+'''
+
+import math
+import sys
+
+print(sys.argv)
+
+sa1 = (math.pow(((int(sys.argv[3]))/2),2)*math.pi)
+if(len(sys.argv)>5):
+    sa2 = (math.pow(((int(sys.argv[5]))/2),2)*math.pi)
+if(len(sys.argv)>7):    
+    sa3 = (math.pow(((int(sys.argv[7]))/2),2)*math.pi)
+
+# Nazwa pizzeri 1, nazwa pizzy 2, rozmiar pizzy 3, cena pizzy 4
+print(f"Restauracja: {sys.argv[1]}, pizza: {sys.argv[2]}, rozmiar: {sys.argv[3]}cm, cena: {sys.argv[4]}zł")
+if(len(sys.argv)>5):
+    print(f"Restauracja: {sys.argv[1]}, pizza: {sys.argv[2]}, rozmiar: {sys.argv[5]}cm, cena: {sys.argv[6]}zł")
+if(len(sys.argv)>7):
+    print(f"Restauracja: {sys.argv[1]}, pizza: {sys.argv[2]}, rozmiar: {sys.argv[7]}cm, cena: {sys.argv[8]}zł")
+
+p1 = float(sys.argv[4])/sa1
+if len(sys.argv)>5 and len(sys.argv)<8:
+    p2 = float(sys.argv[6])/sa2
+    if(p1 > p2):
+        print(f"Najbardziej opłaca się kupić: {sys.argv[5]}cm")
+    else:
+        print(f"Najbardziej opłaca się kupić: {sys.argv[3]}cm")
+if(len(sys.argv)>7):
+    p2 = float(sys.argv[6])/sa2
+    p3 = float(sys.argv[8])/sa3
+    price_list = {p1,p2,p3}
+    if(min(price_list) == p1):
+        print(f"Najbardziej opłaca się kupić: {sys.argv[3]}cm")
+    elif(min(price_list) == p2):
+        print(f"Najbardziej opłaca się kupić: {sys.argv[5]}cm")
+    else:
+        print(f"Najbardziej opłaca się kupić: {sys.argv[7]}cm")
